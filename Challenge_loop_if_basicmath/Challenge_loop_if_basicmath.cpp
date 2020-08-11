3299577186205f92b086ef6b172c1c5c9c8b773b
ภูรินท์ ทับทิมโต <pamzalove20@hotmail.com>
2020-08-11 14:44:23 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+int main()
+{
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	int many = abs(b - a) + 1;
+	int x[1000];
+	int i;
+	if (a < b)
+	{
+		for (i = 0; i < many; i++)
+		{
+			x[i] = a++;
+			printf("%d ", x[i]);
+		}
+	}
+	else
+	{
+		for (i = 0; i < many; i++)
+		{
+			x[i] = a--;
+			printf("%d ", x[i]);
+			
+		}
+	}
+	printf("\n");
+	float average = 0;
+	for (i = 0; i < many; i++)
+	{
+		average += x[i];
+	}
+	average /= (float)many;
+	printf("Average = %.1f\n", average);
+	float sd = 0;
+	for (i = 0; i < many; i++)
+	{
+		sd += pow(x[i] - average, 2);
+	}
+	sd = sqrt(sd / (many - 1));
+	printf("SD = %.2f\n", sd);
+	return 0;
+}
