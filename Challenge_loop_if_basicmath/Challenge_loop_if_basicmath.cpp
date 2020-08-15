2eb2f2b812bc2b2370499647d1e3047c0910839c
ภูรินท์ ทับทิมโต <pamzalove20@hotmail.com>
2020-08-15 11:15:05 +0700

    1

--- Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
+++ Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<math.h>
+float abss(float x)
+{
+	if (x < 0)
+	{
+		return x * -1;
+	}
+	else if (x >= 0)
+	{
+		return x;
+	}
+}
 int main()
 {
 	int a, b;
 	scanf_s("%d %d", &a, &b);
-	float many = abs(b - a) + 1;
+	float many = abss(b - a) + 1;
 	int x[1000];
 	int i;
 	if (a < b)
@@ -16,7 +27,7 @@ int main()
 			printf("%d ", x[i]);
 		}
 	}
-	else
+	else if(a>b)
 	{
 		for (i = 0; i < many; i++)
 		{
@@ -25,20 +36,31 @@ int main()
 			
 		}
 	}
-	printf("\n");
-	float average = 0;
-	for (i = 0; i < many; i++)
+	if (a == b)
 	{
-		average += x[i];
+		float average = a;
+		float sd = 0;
+		printf("%d\n", a);
+		printf("Average = %.1f\n", average);
+		printf("SD = %.2f\n", sd);
 	}
-	average /= many;
-	printf("Average = %.1f\n", average);
-	float sd = 0;
-	for (i = 0; i < many; i++)
+	else
 	{
-		sd += pow(x[i] - average, 2);
+		printf("\n");
+		float average = 0;
+		for (i = 0; i < many; i++)
+		{
+			average += x[i];
+		}
+		average /= many;
+		printf("Average = %.1f\n", average);
+		float sd = 0;
+		for (i = 0; i < many; i++)
+		{
+			sd += pow(x[i] - average, 2);
+		}
+		sd = sqrt(sd / (many - 1));
+		printf("SD = %.2f\n", sd);
 	}
-	sd = sqrt(sd / (many - 1));
-	printf("SD = %.2f\n", sd);
 	return 0;
 }
